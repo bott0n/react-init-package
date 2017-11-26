7c9bcc324f68fdd9ed8017d9c5db59e8c4c31529
jacky2001114 <jacky2001114@gmail.com>
2017-11-26 17:31:21 +0800

    .

--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
-        },
+       
       }
     ],
   },
